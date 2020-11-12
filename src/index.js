7b9c8c4512459690f8bca04cadb88ed4a3fa8554
Yurii <streltsovyurii86@gmal.com>
2020-11-12 03:15:48 +0200

    Запрос и отрисовка списка

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles.css';
+import './js/fetchCountries';
+import API from './js/fetchCountries';
+import dropDownTpl from './templates/dropdownTpl.hbs';
+
+const inputRef = document.querySelector('.js-drop-down-list');
+API.fetchCountries('ukra').then(renderCountriesList);
+
+function renderCountriesList(data) {
+  inputRef.insertAdjacentHTML('beforeend', dropDownTpl(data));
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,6 @@
-export default function fetchCountries(searchQuery) {
-  fetch(`url${searchQuery}`)
-    .then(response => response.json())
-    .then(data => {
-      data;
-    });
+const BASE_URL = 'https://restcountries.eu/rest/v2/name/';
+
+function fetchCountries(searchQuery) {
+  return fetch(`${BASE_URL}${searchQuery}`).then(response => response.json());
 }
+export default { fetchCountries };
