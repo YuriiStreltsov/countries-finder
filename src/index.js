e2b52332d777a8818aabc7c1094b28d70d8346a6
YuriiStreltsov <65507638+YuriiStreltsov@users.noreply.github.com>
2020-11-13 15:46:24 +0200

    Добавляю шаблон разметки контента

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import './styles.css';
 import './js/fetchCountries';
 import API from './js/fetchCountries';
 import dropDownTpl from './templates/dropdownTpl.hbs';
+import contentTpl from './templates/contentTpl.hbs'
 import renderHTML from './js/renderHTML'
 
 const inputRef = document.querySelector('.js-drop-down-list');
-API.fetchCountries('ukraine').then(data => renderHTML(data, inputRef, dropDownTpl));
+const contentContainerRef = document.querySelector('.js-content')
+
+API.fetchCountries('canada').then((data) => { 
+  renderHTML(data, inputRef, dropDownTpl);
+  renderHTML(data, contentContainerRef, contentTpl);
+console.log(contentTpl(data));})
+  
+
+
+
 
 
